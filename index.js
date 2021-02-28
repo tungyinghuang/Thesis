54d5130506747fdeb4a0a336469764848095e674
jennyhihi <jennyh0227@gmail.com>
2021-02-28 16:23:48 -0500

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const Datastore = require ('nedb')
 const app = express();
 const port = process.env.PORT ||3000;
 app.listen(port, ()=> {
-  console.log(`starting server at ${port}` )
+  
 });
 app.use(express.static('public'))
 app.use(express.json({limit:"1mb"}))
